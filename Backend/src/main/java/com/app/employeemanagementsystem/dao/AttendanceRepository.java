2fe69723a20443541dd5514b22fc8ef13e3a13ca
KD2-80527 <nirmalzade29@gmail.com>
2024-02-19 01:11:48 +0530

    some changes done in backend and frontend

--- Backend/src/main/java/com/app/employeemanagementsystem/dao/AttendanceRepository.java
+++ Backend/src/main/java/com/app/employeemanagementsystem/dao/AttendanceRepository.java
@@ -1,8 +1,11 @@
 package com.app.employeemanagementsystem.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.app.employeemanagementsystem.entity.Attendance;
+import com.app.employeemanagementsystem.entity.User;
+
+import java.util.List;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Long> {
+    List<Attendance> findByUser(User user);
 }
--- Backend/src/main/java/com/app/employeemanagementsystem/dao/LeaveRequestRepository.java
+++ Backend/src/main/java/com/app/employeemanagementsystem/dao/LeaveRequestRepository.java
@@ -1,8 +1,11 @@
 package com.app.employeemanagementsystem.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.app.employeemanagementsystem.entity.LeaveRequest;
+import com.app.employeemanagementsystem.entity.User;
+
+import java.util.List;
 
 public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long> {
+    List<LeaveRequest> findByUser(User user);
 }
